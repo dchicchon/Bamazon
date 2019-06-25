30fa7045cf83b3e16d68e498b5dbb55c412b252d
blueDogs <danielchicchon@gmail.com>
2019-06-24 19:18:49 -0700

    added inquirer questions to .js file

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -2,3 +2,19 @@ var sql = require("mysql");
 var inquirer = require("inquirer");
 var Table = require("cli-table");
 
+
+inquirer.prompt([
+    {
+        type: 'number',
+        name: 'id',
+        message: 'Welcome to the store! Which item would you like to buy?'
+    },
+    {
+        type: 'number',
+        name: 'quantity',
+        message: "How many would you like to buy?"
+    }
+]).then(response => {
+    console.log(response.id);
+    console.log(response.quantity);
+})
